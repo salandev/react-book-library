d0dfe0a92e8fb253813a9d4014577cc4f3f42035
Sally Ryan <sal_ryan@hotmail.co.uk>
2018-03-04 21:03:19 +0000

    feat: add map to list out books

---
+++ src/Bookshelf.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class Bookshelf extends Component {
+
+  render() {
+	const { books } = this.props
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+        	<h1>MyReads</h1>
+		</div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+            	<h2 className="bookshelf-title">title</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+      				{books.map((book) => (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                        	<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                    </li>
+      				))}
+                  </ol>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf
