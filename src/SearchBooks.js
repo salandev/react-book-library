61f2e2783feaa1ba87b2badf5335cc3465f29ec8
Sally Ryan <sal_ryan@hotmail.co.uk>
2018-03-05 20:13:17 +0000

    feat: Add search input. Add state and function to grab input value.  Test values entered display in JSON.stringify

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchBooks extends Component {
+  state = {
+    search: []
+  }
+
+  updateQuery = (search) => {
+    this.setState({search: search.trim()})
+  }
 
   render() {
     return (
-      <div>
-        Search Books
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            <input 
+              type="text" 
+              placeholder="Search by title or author"
+              value={ this.state.search }
+              onChange={(event) => (this.updateQuery(event.target.value))}
+            />
+             {JSON.stringify(this.state)} 
+          </div>
+        </div>
+        <div className="search-books-results">
+
+        </div>
       </div>
-    )
-  }
+    )}
 }
 
 export default SearchBooks
