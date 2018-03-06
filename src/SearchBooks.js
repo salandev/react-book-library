9d59bdeb64c0335c530ddcc3db559c4b6416bcf7
Sally Ryan <sal_ryan@hotmail.co.uk>
2018-03-06 09:51:31 +0000

    refactor: Add BooksAPI. Update state and function. List out results

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './utils/BooksAPI'
+import Book from './Book'
 
 class SearchBooks extends Component {
   state = {
-    search: []
+    search: '',
+    searchBooks: []
   }
 
-  updateQuery = (search) => {
-    this.setState({search: search.trim()})
+  updateQuery = (search) => {   
+    this.setState({search: search})
+    if(search.trim()) {
+      BooksAPI.search(search).then((books) => {
+        if(books && books.length > 0) {
+          this.setState({searchBooks: books})
+        } else {
+          this.setState({
+            searchBooks: [],
+          })
+        }
+     })
+    } else {
+      this.setState({
+        search: '',
+        searchBooks: []
+      })
+    }
   }
 
   render() {
+    const { searchBooks, search } = this.state
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -19,14 +39,23 @@ class SearchBooks extends Component {
             <input 
               type="text" 
               placeholder="Search by title or author"
-              value={ this.state.search }
+              value={ search }
               onChange={(event) => (this.updateQuery(event.target.value))}
             />
-             {JSON.stringify(this.state)} 
           </div>
         </div>
         <div className="search-books-results">
-
+			<div>
+              <ol className="books-grid">
+                { searchBooks.map((book) => (
+                <li key={ book.id }>
+                  <Book
+                      book={ book } 
+                  />
+                </li>
+                )) }
+              </ol>
+          	</div>
         </div>
       </div>
     )}
